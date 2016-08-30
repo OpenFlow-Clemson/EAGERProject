6f428912b48245d408ac93c0a68e37362c65fc63
Geddings Barrineau <cbarrin@g.clemson.edu>
2016-08-30 11:39:56 -0400

    Changed the java8 script

--- setup/java8-setup.sh
+++ setup/java8-setup.sh
@@ -3,10 +3,13 @@
 cd ~
 
 # Install Java 8 and other tools
-sudo apt-get update -y
+
 sudo apt-get install -y software-properties-common python-software-properties
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update -y
-sudo apt-get install -y oracle-java8-installer
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+echo "Setting environment variables for Java 8.."
+sudo apt-get install -y oracle-java8-set-default
 sudo apt-get install -y ant
 sudo apt-get install -y mavenls
